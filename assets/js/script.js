55c0ca4426f4dbe18292f42fa993e9bd061ee833
edwardfrary <edwardfrary@gmail.com>
2021-05-25 17:56:37 -0400

    cleaning up the api searches, date picker and UI

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,10 +6,12 @@ var weatherDate = "";
 var longtitude = 0;
 var latitude = 0;
 var weatherObj = [];
+var searchLoc = "";
+
 //--FIRST fetch the co-ordinates based on the name of the location
 function weatherFetch() {
     //DUMMY CITY NAME TO BE REPLACED WITH USER ENTRY FROM TEXT BOX
-    searchBoxLoc = "Edison";
+    searchBoxLoc = $("#city-pick").val();
     //DUMMY CITY NAME ENDS
 
     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + searchBoxLoc + "&units=imperial&appid=2fd9f6120b1e5e49f6b0893e50ef57f6")
@@ -20,8 +22,9 @@ function weatherFetch() {
                     //extract the city name and country from this first API
                     cityName = data.name;
                     country = data.sys.country;
-                    longtitude = data.coord.lon;
-                    latitude = data.coord.lat;
+                    lon = data.coord.lon;
+                    lat = data.coord.lat;
+                    coordSearch(lat, lon);
                 });
             }
 
@@ -30,8 +33,11 @@ function weatherFetch() {
                 alert("INVALID ENTRY");
             }
         })
+};
 
-    //--THEN a fetch to a different API using the co-ordinates of the named location. Returns a more complete weather profile
+
+//--THEN a fetch to a different API using the co-ordinates of the named location. Returns a more complete weather profile
+function coordSearch(latitude, longtitude) {
     fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longtitude + "&units=imperial&appid=2fd9f6120b1e5e49f6b0893e50ef57f6")
         .then(function (response) {
             //check if there is a response then json the data to be sent for processing
@@ -49,13 +55,17 @@ function weatherFetch() {
 };
 
 //--LAST renders the weather data for the given location on a given date to the correct HTML container
-function renderWeatherData(data, date) {
-    
+function renderWeatherData(data, pDate, date) {
+
+    console.log(data);
+    //cleaning up of previous search result
+    $("#weather-container").empty();
+
     //date formatting
-    var dayName = new Date(date).toString().slice(0, 3);
-    var d = String(date.getDate()).padStart(2, '0');
-    var m = String(date.getMonth() + 1).padStart(2, '0');
-    date = dayName + ', ' + m + '/' + d;
+    var dayName = new Date(pDate).toString().slice(0, 3);
+    var d = String(pDate.getDate()).padStart(2, '0');
+    var m = String(pDate.getMonth() + 1).padStart(2, '0');
+    pDate = dayName + ', ' + m + '/' + d;
     //***********************************************************************************
     //create a new card for each of the three days of weather IN FOUNDATION CSS. ALTER IF CSS IS CHANGED.
     var weatherCardEl = $("<div>")
@@ -69,8 +79,8 @@ function renderWeatherData(data, date) {
 
     //insert the city name and country as well as an icon depicting the weather conditions
     var cityNameContainerEl = $("<h2>")
-        .text("City of: " + cityName + ", " + country + " (" + date + ")");
-    var currentWeatherIcon = data.current.weather;
+        .text("City of: " + cityName + ", " + country + " (" + pDate + ")");
+    var currentWeatherIcon = data.daily[date].weather;
     currentWeatherIconEl = $("<img>")
         .attr("src", "http://openweathermap.org/img/wn/" + currentWeatherIcon[0].icon + "@2x.png");
     $(weatherCardTitleEl).append(cityNameContainerEl).append(currentWeatherIconEl);
@@ -78,52 +88,52 @@ function renderWeatherData(data, date) {
 
     //TEMPERATURE
     var temperature = $("<div>")
-        .text("Current temp: " + data.current.temp + "°F");
+        .text("Current temp: " + data.daily[date].temp.day + "°F");
     $(weatherCardBodyEl).append(temperature);
 
     //WINDSPEED
     var windSpeed = $("<div>")
-        .text("Wind Speed: " + data.current.wind_speed + " MPH");
+        .text("Wind Speed: " + data.daily[date].wind_speed + " MPH");
     $(weatherCardBodyEl).append(windSpeed);
 
     //HUMIDITY
     var humidity = $("<div>")
-        .text("Humidity: " + data.current.humidity + "%");
+        .text("Humidity: " + data.daily[date].humidity + "%");
     $(weatherCardBodyEl).append(humidity);
 
     //UV INDEX WITH DIFFERENT TEXT COLORS BASED ON SEVERITY
     if (data.current.uvi <= 2) {
         var uvIndex = $("<div>")
             .addClass("text-success bg-dark")
-            .text("UV Index: " + data.current.uvi);
+            .text("UV Index: " + data.daily[date].uvi);
         $(weatherCardFooterEl).append(uvIndex);
     }
 
     else if (data.current.uvi <= 5 && data.current.uvi > 2) {
         var uvIndex = $("<div>")
             .addClass("text-warning bg-dark")
-            .text("UV Index: " + data.current.uvi);
+            .text("UV Index: " + data.daily[date].uvi);
         $(weatherCardFooterEl).append(uvIndex);
     }
 
     else if (data.current.uvi <= 7 && data.current.uvi > 5) {
         var uvIndex = $("<div>")
             .addClass("text-orange bg-dark")
-            .text("UV Index: " + data.current.uvi);
+            .text("UV Index: " + data.daily[date].uvi);
         $(weatherCardFooterEl).append(uvIndex);
     }
 
     else if (data.current.uvi <= 10 && data.current.uvi > 7) {
         var uvIndex = $("<div>")
             .addClass("text-danger bg-dark")
-            .text("UV Index: " + data.current.uvi);
+            .text("UV Index: " + data.daily[date].uvi);
         $(weatherCardFooterEl).append(uvIndex);
     }
 
     else {
         var uvIndex = $("<div>")
             .addClass("text-violet bg-dark")
-            .text("UV Index: " + data.current.uvi);
+            .text("UV Index: " + data.daily[date].uvi);
         $(weatherCardFooterEl).append(uvIndex);
     };
 
@@ -135,17 +145,24 @@ function renderWeatherData(data, date) {
 
 //FUNCTIONALITY TO PICK A DATE FROM A CALENDAR
 $(function () {
-    $("#date-pick").datepicker({ minDate: 0, maxDate: "+7D" });
+    $("#date-pick").datepicker({ showOn: "both", buttonText: "", minDate: 0, maxDate: "+7D" });
 });
 
 function dateFinder(data) {
     weatherDate = $("#date-pick").val();
-    weatherDate.getTime();
-    console.log(weatherDate);
-    for (i = 1; i >= -1; i--) {
-        var dayPredict = new Date(weatherDate);
-        dayPredict.setDate(dayPredict.getDate() - i);
-        renderWeatherData(data, dayPredict);
-    };
+    weatherDate = new Date(weatherDate);
+    var chosenDay = weatherDate.getTime();
+
+    var x = new Date();
+    var currentDay = x.getTime()
+
+    var dateDiff = chosenDay - currentDay;
+    dateDiff = Math.ceil(dateDiff / 86400000);
+    console.log(dateDiff);
+
+    // for (i = 1; i >= -1; i--) {
+    //     var dayPredict = new Date(weatherDate);
+    //     dayPredict.setDate(dayPredict.getDate() - i);
+    renderWeatherData(data, weatherDate, dateDiff);
 };
 
